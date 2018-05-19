08ba97914940c0cf54498b46995caa17a75de710
Unknown <chriswayg@gmail.com>
2018-05-19 14:38:33 +0800

    FIX 'VM iso spawns getty on serial port by default'
     https://bugs.alpinelinux.org/issues/8704

--- alpine3.7/scripts/01alpine.sh
+++ alpine3.7/scripts/01alpine.sh
@@ -2,3 +2,7 @@ set -ux
 
 # update all packages, especially the kernel.
 apk update && apk upgrade
+
+# FIX 'VM iso spawns getty on serial port by default'
+# https://bugs.alpinelinux.org/issues/8704
+sed -i '/^ttyS0/s/^/#/' /etc/inittab
